58a97cb7ace77093a06f0ecbb57a7b8995898532
leoKV <kevorvs@gmail.com>
2024-01-17 13:24:14 -0600

    navbar ver1.0

---
+++ src/js/expediente-clinico.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+    var menuInicio = document.getElementById('expediente-clinico');
+    menuInicio.style.background='#e32bac';
+    menuInicio.style.color='white';
+    menuInicio.style.textDecoration = 'underline';
+    menuInicio.style.borderRadius='7px';
+}
---
+++ src/js/inicio.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+    var menuInicio = document.getElementById('inicio');
+    menuInicio.style.background='#e32bac';
+    menuInicio.style.color='white';
+    menuInicio.style.textDecoration = 'underline';
+    menuInicio.style.borderRadius='7px';
+}
