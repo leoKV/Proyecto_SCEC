401ff58f768be86294ebd8d9373a01c3d670d2e8
leoKV <kevorvs@gmail.com>
2024-01-18 10:15:48 -0600

    Footer & Login Design

--- src/js/acceder.js
+++
@@ -1,7 +0,0 @@
-window.onload = function(){
-    var menuInicio = document.getElementById('acceder');
-    menuInicio.style.background='#e32bac';
-    menuInicio.style.color='white';
-    menuInicio.style.textDecoration = 'underline';
-    menuInicio.style.borderRadius='7px';
-}
--- src/js/ayuda.js
+++ src/js/ayuda.js
@@ -1,7 +1,10 @@
 window.onload = function(){
     var menuInicio = document.getElementById('ayuda');
-    menuInicio.style.background='#e32bac';
     menuInicio.style.color='white';
     menuInicio.style.textDecoration = 'underline';
     menuInicio.style.borderRadius='7px';
+    menuInicio.style.background = '#bc4e9c';
+    menuInicio.style.background = '-webkit-linear-gradient(to right, #f80759, #bc4e9c)';
+    menuInicio.style.background = 'linear-gradient(to right, #f80759, #bc4e9c)';
+    menuInicio.style.border = '1px solid white';
 }
